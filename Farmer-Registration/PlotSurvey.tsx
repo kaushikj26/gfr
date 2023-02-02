cb6a3ca824b92f0c844e7743bd6d051c8e3dd8e8
Kaushik J <115524153+kaushikj26@users.noreply.github.com>
2023-02-02 14:45:12 +0530

    Add files via upload

--- Farmer-Registration/PlotSurvey.tsx
+++ Farmer-Registration/PlotSurvey.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import FormLabel from '@mui/material/FormLabel';
 import { Formik, Form, Field } from "formik";
-import { Alert, Autocomplete, Button, FormControl, FormControlLabel, FormHelperText, InputLabel, MenuItem, Radio, RadioGroup, Select, Snackbar, Stack } from '@mui/material';
+import { Alert, Autocomplete, Button, FormControl, FormControlLabel, FormHelperText, InputLabel, MenuItem, Radio, RadioGroup, Select, InputAdornment,  Snackbar, Stack } from '@mui/material';
 import * as Yup from 'yup';
 import { redirect, useNavigate,useParams } from "react-router-dom";
 // import { Country, State, City } from "country-state-city";
@@ -20,15 +21,15 @@ const PlotSurvey:React.FC=()=>{
     const validationSchema=Yup.object({
         location: Yup.string().required('Required'),
         surveyNum: Yup.string().required('Required'),
-        soilClr: Yup.string().required('Required'),
+        soilColor: Yup.string().required('Required'),
         soilType: Yup.string().required('Required'),
         area: Yup.string().required('Required'),
-        curCrop: Yup.string().required('Required'),
+        currCrop: Yup.string().required('Required'),
         qrCode: Yup.string().required('Required'),
         plotImg: Yup.string().required('Required'),
         prevCrop: Yup.string().required('Required'),
         stoniness: Yup.string().required('Required'),
-        slopping: Yup.string().required('Required'),
+        landslopping: Yup.string().required('Required'),
         drainage: Yup.string().required('Required'),
         waterSource: Yup.string().required('Required'),
         waterCap: Yup.string().required('Required'),
@@ -41,7 +42,7 @@ const PlotSurvey:React.FC=()=>{
         targetYield: Yup.string().required('Required'),
         yieldPrice: Yup.string().required('Required'),
         manureQuantity: Yup.string().required('Required'),
-        prevFerti: Yup.string().required('Required')
+        // prevFerti: Yup.string().required('Required')
     })
   
     return(
@@ -51,17 +52,17 @@ const PlotSurvey:React.FC=()=>{
              
     <Formik 
     initialValues={{
-        location: '',Yup.string().required('Required')
+        location: '',
         surveyNum: '',
-        soilClr: '',
+        soilColor: '',
         soilType: '',
         area: '',
-        curCrop: '',
+        currCrop: '',
         qrCode: '',
         plotImg: '',
         prevCrop: '',
         stoniness: '',
-        slopping: '',
+        landslopping: '',
         drainage: '',
         waterSource: '',
         waterCap: '',
@@ -89,92 +90,6 @@ const PlotSurvey:React.FC=()=>{
       fullWidth
       component={TextField}
       type="text"
-      name="name"
-      onChange={(e:any)=>{
-        formik.setFieldValue("name",e.target.value)
-      }}
-      onBlur={formik.handleBlur}
-      value={formik.values.name}
-      helperText={formik.errors.name}
-      error={ formik.errors.name ? true : false}
-    label="Name" variant="outlined"
-    />
-    </Grid>
-
-    <Grid item xs={12} md={6}>
-    <Field
-      fullWidth
-      component={TextField}
-      type="text"
-      name="phNumber"
-      onChange={(e:any)=>{
-        formik.setFieldValue("phNumber",e.target.value)
-      }}
-      onBlur={formik.handleBlur}
-      value={formik.values.phNumber}
-      helperText={formik.errors.phNumber}
-      error={ formik.errors.phNumber ? true : false}
-      label="Phone Number*" variant="outlined"
-    />
-    </Grid>
-
-    <Grid item xs={12} md={6}>
-    <Field
-      fullWidth
-      component={TextField}
-      type="text"
-      name="email"
-      onChange={(e:any)=>{
-        formik.setFieldValue("email",e.target.value)
-      }}
-      onBlur={formik.handleBlur}
-      value={formik.values.email}
-      helperText={formik.errors.email}
-      error={ formik.errors.email ? true : false}
-       label="Email" variant="outlined"
-    />
-    </Grid>
-
-    <Grid item xs={12} md={6}>
-    <Field
-      fullWidth
-      component={TextField}
-      type="text"
-      name="adhaar"
-      onChange={(e:any)=>{
-        formik.setFieldValue("adhaar",e.target.value)
-      }}
-      onBlur={formik.handleBlur}
-      value={formik.values.adhaar}
-      helperText={formik.errors.adhaar}
-      error={ formik.errors.adhaar ? true : false}
-      label="Adhaar" variant="outlined"
-    />
-    </Grid>
-
-    <Grid  item xs={12} md={6}>
-    <FormControl fullWidth>
-    <InputLabel id="Language">Language</InputLabel>
-        <Select
-            labelId="Language"
-            id="language"
-            value={formik.values.language}
-            label="Language"
-            onChange={(e:any)=>{
-                formik.setFieldValue("language",e.target.value)
-            }}
-        >
-        <MenuItem value={1}>English</MenuItem>
-        <MenuItem value={0}>Kannada</MenuItem>
-        </Select>
-    </FormControl>
-    </Grid>
-
-    {/* <Grid item xs={12} md={6}>
-    <Field
-      fullWidth
-      component={TextField}
-      type="text"
       name="location"
       onChange={(e:any)=>{
         formik.setFieldValue("location",e.target.value)
@@ -183,26 +98,415 @@ const PlotSurvey:React.FC=()=>{
       value={formik.values.location}
       helperText={formik.errors.location}
       error={ formik.errors.location ? true : false}
-      label="Location" variant="outlined"
+    label="Location" variant="outlined"
     />
-    </Grid> */}
+    </Grid>
 
     <Grid item xs={12} md={6}>
     <Field
       fullWidth
       component={TextField}
       type="text"
-      name="address"
+      name="surveyNum"
       onChange={(e:any)=>{
-        formik.setFieldValue("address",e.target.value)
+        formik.setFieldValue("surveyNum",e.target.value)
       }}
       onBlur={formik.handleBlur}
-      value={formik.values.address}
-      helperText={formik.errors.address}
-      error={ formik.errors.address ? true : false}
-      label="Address" variant="outlined"
+      value={formik.values.surveyNum}
+      helperText={formik.errors.surveyNum}
+      error={ formik.errors.surveyNum ? true : false}
+      label="Survey Number" variant="outlined"
     />
     </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl fullWidth>
+    <InputLabel id="SoilColor">Soil Colour</InputLabel>
+        <Select
+            labelId="SoilColor"
+            id="soilColor"
+            value={formik.values.soilColor}
+            label="SoilColor"
+            onChange={(e:any)=>{
+                formik.setFieldValue("soilColor",e.target.value)
+            }}
+        >
+        <MenuItem value={1}>colour1</MenuItem>
+        <MenuItem value={0}>colour2</MenuItem>
+        </Select>
+    </FormControl>
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl fullWidth>
+    <InputLabel id="SoilType">Soil Type</InputLabel>
+        <Select
+            labelId="SoilType"
+            id="soilType"
+            value={formik.values.soilType}
+            label="SoilType"
+            onChange={(e:any)=>{
+                formik.setFieldValue("soilType",e.target.value)
+            }}
+        >
+        <MenuItem value={1}>SoilType 1</MenuItem>
+        <MenuItem value={0}>SoilType 2</MenuItem>
+        </Select>
+    </FormControl>
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+    <Field
+      fullWidth
+      component={TextField}
+      type="text"
+      name="area"
+      onChange={(e:any)=>{
+        formik.setFieldValue("area",e.target.value)
+      }}
+      onBlur={formik.handleBlur}
+      value={formik.values.area}
+      helperText={formik.errors.area}
+      error={ formik.errors.area ? true : false}
+       label="Area" variant="outlined"
+    />
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl fullWidth>
+    <InputLabel id="CurrCrop">Current Crop</InputLabel>
+        <Select
+            labelId="CurrCrop"
+            id="currCrop"
+            value={formik.values.currCrop}
+            label="currCrop"
+            onChange={(e:any)=>{
+                formik.setFieldValue("currCrop",e.target.value)
+            }}
+        >
+        <MenuItem value={1}>Current Crop 1</MenuItem>
+        <MenuItem value={0}>Current Crop 2</MenuItem>
+        </Select>
+    </FormControl>
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+    <Field
+      fullWidth
+      component={TextField}
+      type="text"
+      name="qrCode"
+      onChange={(e:any)=>{
+        formik.setFieldValue("qrCode",e.target.value)
+      }}
+      onBlur={formik.handleBlur}
+      value={formik.values.qrCode}
+      helperText={formik.errors.qrCode}
+      error={ formik.errors.qrCode ? true : false}
+      label="QR Code" variant="outlined"
+    />
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+    <Field
+      fullWidth
+      component={TextField}
+      type="text"
+      name="plotImg"
+      onChange={(e:any)=>{
+        formik.setFieldValue("plotImg",e.target.value)
+      }}
+      onBlur={formik.handleBlur}
+      value={formik.values.plotImg}
+      helperText={formik.errors.plotImg}
+      error={ formik.errors.plotImg ? true : false}
+      label="Plot Image" variant="outlined"
+    />
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl fullWidth>
+    <InputLabel id="PrevCrop">Previous Crop</InputLabel>
+        <Select
+            labelId="PrevCrop"
+            id="prevCrop"
+            value={formik.values.prevCrop}
+            label="prevCrop"
+            onChange={(e:any)=>{
+                formik.setFieldValue("prevCrop",e.target.value)
+            }}
+        >
+        <MenuItem value={1}>Previous Crop 1</MenuItem>
+        <MenuItem value={0}>Previous Crop 2</MenuItem>
+        </Select>
+    </FormControl>
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl>
+      <FormLabel id="demo">Stoniness</FormLabel>
+      <RadioGroup
+        row
+        name="stoniness"
+        value={formik.values.stoniness}
+        onChange={(e:any)=>{
+          formik.setFieldValue("stoniness",e.target.value)
+      }}
+      >
+        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="no" control={<Radio />} label="No" />
+        <FormControlLabel value="unsure" control={<Radio />} label="Unsure" />
+      </RadioGroup>
+    </FormControl>
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl>
+      <FormLabel id="demo">Land Sloping</FormLabel>
+      <RadioGroup
+        row
+        name="landslopping"
+        value={formik.values.landslopping}
+        onChange={(e:any)=>{
+          formik.setFieldValue("landslopping",e.target.value)
+      }}
+      >
+        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="no" control={<Radio />} label="No" />
+        <FormControlLabel value="unsure" control={<Radio />} label="Unsure" />
+      </RadioGroup>
+    </FormControl>
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl fullWidth>
+    <InputLabel id="Drainage">Drainage</InputLabel>
+        <Select
+            labelId="Drainage"
+            id="drainage"
+            value={formik.values.drainage}
+            label="drainage"
+            onChange={(e:any)=>{
+                formik.setFieldValue("drainage",e.target.value)
+            }}
+        >
+        <MenuItem value={1}>Drainage 1</MenuItem>
+        <MenuItem value={0}>Drainage 2</MenuItem>
+        </Select>
+    </FormControl>
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl fullWidth>
+    <InputLabel id="WaterSource">Water Source</InputLabel>
+        <Select
+            labelId="WaterSource"
+            id="waterSource"
+            value={formik.values.waterSource}
+            label="waterSource"
+            onChange={(e:any)=>{
+                formik.setFieldValue("waterSource",e.target.value)
+            }}
+        >
+        <MenuItem value={1}>Water Source 1</MenuItem>
+        <MenuItem value={0}>Water Source 2</MenuItem>
+        </Select>
+    </FormControl>
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl fullWidth>
+    <InputLabel id="WaterCap">Water Source Capacity</InputLabel>
+        <Select
+            labelId="WaterCap"
+            id="waterCap"
+            value={formik.values.waterCap}
+            label="waterCap"
+            onChange={(e:any)=>{
+                formik.setFieldValue("waterCap",e.target.value)
+            }}
+        >
+        <MenuItem value={1}>Water Capacity 1</MenuItem>
+        <MenuItem value={0}>Water Capacity 2</MenuItem>
+        </Select>
+    </FormControl>
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl>
+      <FormLabel id="demo">Electricity Connection At Farm</FormLabel>
+      <RadioGroup
+        row
+        name="elecFarm"
+        value={formik.values.elecFarm}
+        onChange={(e:any)=>{
+          formik.setFieldValue("elecFarm",e.target.value)
+      }}
+      >
+        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="no" control={<Radio />} label="No" />
+      </RadioGroup>
+    </FormControl>
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl>
+      <FormLabel id="demo">Electricity Phase</FormLabel>
+      <RadioGroup
+        row
+        name="elecPhase"
+        value={formik.values.elecPhase}
+        onChange={(e:any)=>{
+          formik.setFieldValue("elecPhase",e.target.value)
+      }}
+      >
+        <FormControlLabel value="1 Phase" control={<Radio />} label="1 Phase" />
+        <FormControlLabel value="3 Phase" control={<Radio />} label="3 Phase" />
+      </RadioGroup>
+    </FormControl>
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl>
+      <FormLabel id="demo">Irrigation Pump</FormLabel>
+      <RadioGroup
+        row
+        name="irrigPump"
+        value={formik.values.irrigPump}
+        onChange={(e:any)=>{
+          formik.setFieldValue("irrigPump",e.target.value)
+      }}
+      >
+        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="no" control={<Radio />} label="No" />
+        </RadioGroup>
+    </FormControl>
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+    <Field
+      fullWidth
+      component={TextField}
+      type="number"
+      name="pumpSize"
+      onChange={(e:any)=>{
+        formik.setFieldValue("pumpSize",e.target.value)
+      }}
+      onBlur={formik.handleBlur}
+      value={formik.values.pumpSize}
+      helperText={formik.errors.pumpSize}
+      error={ formik.errors.pumpSize ? true : false}
+      label="Pump Size in HP" variant="outlined"
+    />
+    </Grid>
+
+    <Grid  item xs={12} md={6}>
+    <FormControl fullWidth>
+    <InputLabel id="DtConnec">DT Connection</InputLabel>
+        <Select
+            labelId="DtConnec"
+            id="dtConnec"
+            value={formik.values.dtConnec}
+            label="dtConnec"
+            onChange={(e:any)=>{
+                formik.setFieldValue("dtConnec",e.target.value)
+            }}
+        >
+        <MenuItem value={1}>DT Connection 1</MenuItem>
+        <MenuItem value={0}>DT Connection 2</MenuItem>
+        </Select>
+    </FormControl>
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+    <Field
+      fullWidth
+      component={TextField}
+      type="number"
+      name="prevYield"
+      onChange={(e:any)=>{
+        formik.setFieldValue("prevYield",e.target.value)
+      }}
+      onBlur={formik.handleBlur}
+      value={formik.values.prevYield}
+      helperText={formik.errors.prevYield}
+      error={ formik.errors.prevYield ? true : false}
+      label="Previous Yield" variant="outlined"
+    />
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+    <Field
+      fullWidth
+      component={TextField}
+      type="text"
+      name="targetYield"
+      onChange={(e:any)=>{
+        formik.setFieldValue("targetYield",e.target.value)
+      }}
+      onBlur={formik.handleBlur}
+      value={formik.values.targetYield}
+      helperText={formik.errors.targetYield}
+      error={ formik.errors.targetYield ? true : false}
+      label="Target Yield" variant="outlined"
+    />
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+    <Field
+      fullWidth
+      component={TextField}
+      type="number"
+      name="yieldPrice"
+      onChange={(e:any)=>{
+        formik.setFieldValue("yieldPrice",e.target.value)
+      }}
+      onBlur={formik.handleBlur}
+      value={formik.values.yieldPrice}
+      helperText={formik.errors.yieldPrice}
+      error={ formik.errors.yieldPrice ? true : false}
+      label="Previous yield price(INR/Quintal) " variant="outlined"
+      InputProps={{
+        startAdornment: 
+        <InputAdornment position="start">₹</InputAdornment>,
+    }}
+    />
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+    <Field
+      fullWidth
+      component={TextField}
+      type="text"
+      name="manureQuantity"
+      onChange={(e:any)=>{
+        formik.setFieldValue("manureQuantity",e.target.value)
+      }}
+      onBlur={formik.handleBlur}
+      value={formik.values.manureQuantity}
+      helperText={formik.errors.manureQuantity}
+      error={ formik.errors.manureQuantity ? true : false}
+      label="How many trollies of manure were added to the plot in the previous season? " variant="outlined"
+    />
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+    <Field
+      fullWidth
+      component={TextField}
+      type="number"
+      name="prevFerti"
+      onChange={(e:any)=>{
+        formik.setFieldValue("prevFerti",e.target.value)
+      }}
+      onBlur={formik.handleBlur}
+      value={formik.values.prevFerti}
+      helperText={formik.errors.prevFerti}
+      error={ formik.errors.prevFerti ? true : false}
+      label="Fertilizer applied in previous season(Optional)" variant="outlined"
+    />
+    </Grid>
+
 </Grid>
 
 
@@ -233,4 +537,4 @@ const PlotSurvey:React.FC=()=>{
     )
 }
 
-export default FarmerRegistration
+export default PlotSurvey
